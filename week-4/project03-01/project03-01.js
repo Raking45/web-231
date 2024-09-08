df54aa0b970b007c785478a0a2e19ac072437cc8
Robert King <109000063+Raking45@users.noreply.github.com>
2024-09-08 17:41:41 -0400

    Finished Project 03-01

---
+++ week-4/project03-01/project03-01.js
@@ -0,0 +1,34 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Robert King
+      Date:   September 8, 2024
+
+      Filename: project03-01.js
+*/
+
+//variable for menu
+let menuItems = document.getElementsByClassName("menuItem");
+
+//Function for calcTotal
+for (var i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", calcTotal);
+}
+
+    function calcTotal() {
+      let orderTotal = 0;
+
+      for (let i = 0; i < menuItems.length; i++) {
+        if (menuItems[i].checked) {
+          orderTotal += Number(menuItems[i].value);
+        }
+      }
+      document.getElementById("billTotal").innerHTML = formatCurrency(orderTotal);
+    }
+
+ // Function to display a numeric value as a text string in the format $##.##
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
